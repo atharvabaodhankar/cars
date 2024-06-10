58af4f27aa41cf6a37eda83b49d325143f440fb8
AtharvaPC <baodhankaratharva@gmail.com>
2024-06-10 14:12:29 +0530

    added GSAP

--- script.js
+++ script.js
@@ -62,8 +62,177 @@ if (window.matchMedia("(min-width: 768px)").matches)
     duration: 1,
   });
   }
-Shery.makeMagnet(".ride-img" , {
+Shery.makeMagnet(".ride-img , .footer-links ul li a , .Atharva , .navigators li" , {
   ease: "cubic-bezier(0.23, 1, 0.320, 1)",
   duration: 1,
 });
+
+var heroTl = gsap.timeline();
+
+heroTl.from(".loader-h1 span", {
+  y: 100,
+  rotation: 10,
+  duration: 2,
+  stagger: 0.1,
+  ease: "power3.inOut"
+});
+
+heroTl.to("#loader", {
+  top : "-100%",
+  opacity: 0,
+  duration : 1,
+  ease: "power3.inOut"
+})
+heroTl.to("#loader", {
+  display : "none"
+})
+heroTl.from(".navigators li", {
+  opacity: 0,
+  stagger: .4,
+  y : 20,
+  rotation: 10,
+}, 'stagNav')
+
+heroTl.from(".hero h1", {
+  scale: 1.5,
+  y : 300,
+  opacity : 0,
+}, 'stagNav')
+
+heroTl.from(".hero p", {
+  scale: 1.5,
+  y: 300,
+  stagger : 0.5,
+  opacity : 0,
+})
+
+var heading = document.querySelectorAll(".heading");
+
+heading.forEach((ele)=>{
+  gsap.from(ele, {
+    scale: 1.5,
+    y : 300,
+    opacity: 0,
+    stagger : .3,
+    scrollTrigger : ele
+  })
+})
+gsap.from(".insurance-img", {
+  x: 100,
+  opacity: 0,
+  scale : 0.5,
+  scrollTrigger: {
+    trigger: ".insurance-img",
+    scrub : 2,
+    start: "0% 70%",
+    end: "0% center",
+  }
+  
+})
+gsap.from(".faq-menu li", {
+  x: -200,
+  opacity: 0,
+  stagger : 0.5,
+  scrollTrigger : ".faq-menu li"
+})
+gsap.from(".ride-text span", {
+   y : 100,
+  opacity: 0,
+  stagger : 0.3,
+  scrollTrigger : ".ride-text"
+})
+gsap.from(".ride-img", {
+  opacity: 0,
+  scale: .5,
+  scrollTrigger: {
+    trigger: ".ride",
+    scrub : 2,
+    start: "0% 70%",
+    end: "50% center",
+  }
+})
+gsap.from(".showcase-box", {
+  opacity: 0,
+  scale: .5,
+  stagger : -.4,
+  scrollTrigger: {
+    trigger: ".showcase-box",
+    scrub : 3,
+    start: "0% 80%",
+    end: "50% center",
+  }
+})
+
+gsap.from("form", {
+  x: 200,
+  opacity: 0,
+  rotation : 10,
+  duration : 1,
+  stagger : 0.5,
+  scrollTrigger : ".contact"
+})
+gsap.from(".contact", {
+  x: -200,
+  opacity: 0,
+  rotation : -10,
+  duration : 1,
+  stagger : 0.5,
+  scrollTrigger : ".contact"
+})
+gsap.from(".footer", {
+  x: 200,
+  opacity: 0,
+  rotation : -10,
+  duration : 1,
+  stagger : 0.5,
+  scrollTrigger : "#footer"
+})
+gsap.from(".footer-links", {
+  x: -200,
+  opacity: 0,
+  rotation : 10,
+  duration : 1,
+  stagger : 0.5,
+  scrollTrigger : "#footer"
+})
+gsap.from(".features-box", {
+  x: -200,
+  opacity: 0,
+  rotation : 50,
+  duration : 1,
+  stagger: 0.5,
+  ease : "power1.inOut",
+  scrollTrigger : ".features-box"
+})
+gsap.from(".best-right", {
+  x: 200,
+  opacity: 0,
+  duration : 1,
+  ease : "power1.inOut",
+  scrollTrigger : ".best-right"
+})
+gsap.from(".companies h1", {
+  x: 200,
+  opacity: 0,
+  duration : 1,
+  ease : "power1.inOut",
+  scrollTrigger: {
+    trigger: ".companies",
+    scrub : 3,
+    start: "0% 80%",
+    end: "50% center",
+  }
+})
+gsap.from(".companies h2", {
+  x: -200,
+  opacity: 0,
+  duration : 1,
+  ease : "power1.inOut",
+  scrollTrigger: {
+    trigger: ".companies",
+    scrub : 3,
+    start: "0% 80%",
+    end: "50% center",
+  }
+})
 
